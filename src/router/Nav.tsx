70ebabf124aeeb3304e912cb3951b06e3ebb6096
quinn6915 <quinn6915@gmail.com>
2021-07-08 11:17:58 +0300

    nav effect

--- src/router/Nav.tsx
+++ src/router/Nav.tsx
@@ -14,8 +14,8 @@ export default function Nav() {
 
   return (
     <div>
-      <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <nav className="bg-gray-800 sticky top-0 z-50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
