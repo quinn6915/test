87d77ff2c68f0550de652375dd10b34bc72361f7
quinn6915 <quinn6915@gmail.com>
2021-07-08 17:05:40 +0300

    background init post

--- src/layout/PrivatePage.tsx
+++ src/layout/PrivatePage.tsx
@@ -9,7 +9,20 @@ const PrivatePage: React.FC<props> = ({ children }) => {
   return (
     <>
       <Nav />
-      {children}
+      <div>
+        <div className="sticky top-12" style={{ zIndex: -1 }}>
+          <div className="flex justify-center">
+            <div className="h-96 bg-gradient-to-br from-yellow-400  to-yellow-800 w-4/5 rounded-3xl transform -rotate-6 absolute mt-16"></div>
+          </div>
+          <div className="flex justify-center">
+            <div
+              className="h-96 bg-gray-300 w-4/5 rounded-3xl transform -rotate-3 absolute mt-16"
+              style={{ zIndex: -2 }}
+            ></div>
+          </div>
+        </div>
+        {children}
+      </div>
     </>
   );
 };
